6396c7d5193272279df9dae63228e15a97c38f2e
aoster-mira <78741753+aoster-mira@users.noreply.github.com>
2022-04-06 12:04:02 -0400

    update local-cluster install script

--- scripts/local-cluster-setup.sh
+++ scripts/local-cluster-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
 
 wget https://binary.mirantis.com/releases/get_container_cloud.sh
 chmod 0755 get_container_cloud.sh
 ./get_container_cloud.sh
 cd kaas-bootstrap
-echo "Download a new license file from https://container-cloud.mirantis.com/. Click download download license under Try On-Prem."
+echo "Download a new license file by logging in or creating an accunt at https://container-cloud.mirantis.com/. After log in Click download download license under Try On-Prem."
 read -p "Paste contents of mirantis license file: " mirantis_lic
 echo $mirantis_lic > mirantis.lic
 
@@ -14,12 +16,22 @@ export AWS_ACCESS_KEY_ID=AAAAEXAMPLEXXX
 export AWS_SECRET_ACCESS_KEY=ExAMpLE123XXXX987123
 export KAAS_AWS_ENABLED=true
 
-echo "Wait for the bootstrap to get as far as validating the AWS credentials, you should see a log message like: \n"
-echo "`credential default/cloud-config not valid yet` \n"
-echo "When you see the above message, use CTRL + c to quit the bootstrap.\n"
-read -p "Are you ready to continue? (y/n): " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    ./bootstrap.sh all
-fi
+echo "Wait for the bootstrap to get as far as validating the AWS credentials, you should see a log message like:"
+echo "credential default/cloud-config not valid yet"
+echo "When you see the above message, use CTRL + c to quit the bootstrap."
+echo ""
+
+./bootstrap.sh all &
+while :
+do
+  echo " Starting up....."
+  if grep -q "credential default/cloud-config not valid yet" ./logs/kaas-bootstrap-cluster.log; then
+    echo "Local boostratp cluster created successfully. "
+    echo "creating kubeconfig file..."
+    ./bin/kind get kubeconfig --name=clusterapi > ~/mgmt-kc.yml
+    echo "applying templates ...."
+    ./bin/kubectl --kubeconfig ~/mgmt-kc.yml create -f ../templates/full-cluster/docs-template.yml
+    exit
+  fi
+  sleep 5
+done
